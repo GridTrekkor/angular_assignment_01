fb47864bb1f03cb8f7896e36bbaec4f29be0acf5
Geoffrey Hofman-Frethem <gfrethem@gmail.com>
2015-09-28 15:51:31 -0500

    Initial functionality completed.

--- client/app.js
+++ client/app.js
@@ -3,17 +3,41 @@
  */
 var app = angular.module('prezPicker3000', []);
 
-app.controller('MainController', ['$scope', function($scope) {
+app.controller('MainController', ['$scope', '$http', function($scope, $http) {
+$scope.DemWinner ="";
+
+$http({
+    method: 'GET',
+    url: '/getDems'
+}).then(function (response) {
+    $scope.democrats = response.data;
+    $scope.DemWinner = $scope.democrats[randomNumber(0,4)];
+
+});
+
+$http({
+    method: 'GET',
+    url: '/getReps'
+}).then(function (response) {
+    $scope.republicans = response.data;
+    $scope.RepWinner = $scope.republicans[randomNumber(0,4)];
+});
+
+var randomNumber = function(min, max) {
+    return Math.floor(Math.random() * (1 + max - min) + min);
+};
+
+////var candidateNumber = randomNumber(0,4);
+//var demOrRep = randomNumber(0,1);
+
+if (randomNumber(0,1) == 0) {
+    $scope.demsWin = true;
+} else {
+    $scope.repsWin = true;
+}
 
 
-
-
-
-
-
-
-
-
-
+//
+//console.log($scope.DemWinner);
 
 }]);
