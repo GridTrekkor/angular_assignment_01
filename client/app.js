f3e187f5fad5411ed68d30a02f3c7b6970ad08c7
Geoffrey Hofman-Frethem <gfrethem@gmail.com>
2015-09-28 14:21:32 -0500

    init commit

---
+++ client/app.js
@@ -0,0 +1,19 @@
+/**
+ * Created by gfrethem on 9/28/15.
+ */
+var app = angular.module('prezPicker3000', []);
+
+app.controller('MainController', ['$scope', function($scope) {
+
+
+
+
+
+
+
+
+
+
+
+
+}]);
